d1bad68341d46ba1f821b972ba252a47b950e005
Nikos Roussos <nikos.roussos@greenpeace.org>
2026-05-05 10:43:17 +0300

    Disable Google plugin redirection
    [skip ci]

--- tasks/post-deploy/99-testing-tasks.sh
+++ tasks/post-deploy/99-testing-tasks.sh
@@ -2,6 +2,7 @@
 
 echo "Allow plain login for testing..."
 wp option patch delete planet4_features enforce_sso || true
+wp option patch delete galogin ga_auto_login || true
 
 echo "Sync ElasticPress..."
 wp elasticpress sync --setup --force --yes || true
